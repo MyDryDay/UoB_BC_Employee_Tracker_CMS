6cf9279e9f420c3ccc5de0cc8b717ab363f40b2d
George Cope <G.Cope13@gmail.com>
2021-03-11 14:00:23 +0000

    Added npm dependencies to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// Require dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const cTable = require('console.table');
+
+// Require classes
+// !!! Write classes & include here !!!
+
