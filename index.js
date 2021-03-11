468f0e4fe228144c2c4aa34913856d56667f6f17
George Cope <G.Cope13@gmail.com>
2021-03-11 14:41:20 +0000

    Established connection in index.js & added some commands to display all data for each table in seed.sql

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const cTable = require('console.table');
 // Require classes
 // !!! Write classes & include here !!!
 
+// Establish connection to server
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw error;
+    console.log(`Connected as ID: ${connection.threadId}`);
+    connection.end();
+});
