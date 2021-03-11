71c80f8d0f5b847e361e267caa29ee0dc5b82c7f
George Cope <G.Cope13@gmail.com>
2021-03-11 17:06:46 +0000

    Added variables to hold inquirer questions

--- index.js
+++ index.js
@@ -8,6 +8,119 @@ const Departments = require('./lib/department');
 const Employees = require('./lib/employee');
 const Roles = require('./lib/role');
 
+// Variables for tables from database
+let rolesTable;
+let departmentsTable;
+let employeesTable;
+
+// Question arrays for inquirer prompts
+const initialPrompt = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'initialChoices',
+        choices: [
+            {
+                name: 'View all departments',
+                value: 'viewDepartments'
+            },
+            {
+                name: 'View all roles',
+                value: 'viewRoles'
+            },
+            {
+                name: 'View all employees',
+                value: 'viewEmployees'
+            },
+            {
+                name: 'Add a department',
+                value: 'addDepartment'
+            },
+            {
+                name: 'Add a role',
+                value: 'addRole'
+            },
+            {
+                name: 'Add an employee',
+                value: 'addEmployee'
+            },
+            {
+                name: 'Update a role',
+                value: 'updateRole'
+            },
+            {
+                name: 'Exit',
+                value: 'exitApp'
+            }
+        ]
+    }
+];
+
+const addDepartmentPropmpt = {
+    type: 'input',
+    message: 'What is the new department\'s name?',
+    name: 'deptName'
+};
+
+const addEmployeePrompt = [
+    {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'fName'
+    },
+    {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'lName'
+    },
+    {
+        type: 'list',
+        message: 'What is the current role ID of the employee?',
+        name: 'roleID',
+        choices: [1, 2, 3, 4, 5, 6]
+    },
+    {
+        type: 'list',
+        message: 'What is the employee\'s manager\'s ID?',
+        name: 'managerID',
+        choices: [1, 3, 5]
+    }
+];
+
+const addRolePrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the new role?',
+        name: 'role'
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the new role? (Whole numbers only)',
+        name: 'salary'
+    },
+    {
+        type: 'list',
+        message: 'What is the department ID the new role belongs to?',
+        name: 'departmentID',
+        choices: [1, 2, 3]
+    }
+];
+
+const updateRolePrompt = [
+    {
+        type: 'list',
+        message: 'Which employee\'s role would you like to update?',
+        name: 'employeeID',
+        choices: [1, 2, 3, 4, 5, 6]
+    },
+    {
+        type: 'list',
+        message: 'What is the new role of the employee?',
+        name: 'roleID',
+        choices: [1, 2, 3, 4, 5, 6]
+    }
+];
+
 // Establish connection to server
 const connection = mysql.createConnection({
     host: 'localhost',
